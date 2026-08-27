61c8f1b0380f8c99b8acb3465281d6d69da1917f
RafaSan2 <rafaelsanchez@aocgas.com>
2026-08-27 14:57:51 -0600

    Actualizacion de pantalla

--- data.js
+++ data.js
@@ -992,7 +992,7 @@
                                      }
                   },
     "metadata":  {
-                     "fecha_actualizacion":  "2026-08-27 14:53:47",
+                     "fecha_actualizacion":  "2026-08-27 14:56:13",
                      "fecha_reporte":  "Ago 2026"
                  }
 };
